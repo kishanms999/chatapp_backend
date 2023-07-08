35b56fc49364009d53cbbcdde18f87d6be2caf56
kishanms999 <kishanms99@gmail.com>
2023-07-08 19:41:56 +0530

    login backend

--- controllers/user.js
+++ controllers/user.js
@@ -43,6 +43,39 @@ const signup = async (req,res,next)=>{
     }
 }
 
+const generateAccessToken=(id,name)=>{
+    return jwt.sign({userId:id,name:name},process.env.TOKEN_SECRET);
+}
+
+const login = async (req,res,next)=>{
+    try{
+        const {email,password}=req.body;
+        if(isstringinvalid(email) || isstringinvalid(password) ){
+            return res.status(400).json({message:"Emailid or password is missing", success:false})
+        }
+        const user = await User.findAll({where:{email}});
+        if(user.length>0){
+            bcrypt.compare(password,user[0].password,(err,result)=>{
+                if(err){
+                    throw new Error('Something went wrong')
+                }
+                if(result === true){
+                    res.status(200).json({success:true, message:"User logged in successfully",token:generateAccessToken(user[0].id,user[0].name)})
+                } else{
+                    res.status(400).json({success:false, message:"Password is incorrect"})
+                } 
+            })
+            
+        } else{
+            res.status(404).json({success:false, message:"User does not exist"})
+        }
+    } catch(err){
+        res.status(500).json({message:err,success:false})
+    }
+}
+
 module.exports = {
-    signup
+    signup,
+    login,
+    generateAccessToken
 }
